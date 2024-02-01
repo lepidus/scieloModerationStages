0d84b20dc4977d35c6b0b4cbaaa669ad212ba6f9
Jhon <jhon@lepidus.com.br>
2024-02-01 10:46:45 -0400

    Tries wait for jquery loading on findSubmission command
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -56,7 +56,8 @@ Cypress.Commands.add('createSubmission', function (submissionData) {
 });
 
 Cypress.Commands.add('findSubmission', function(tab, title) {
-	cy.get('#' + tab + '-button').click();
+	cy.waitJQuery();
+    cy.get('#' + tab + '-button').click();
     cy.get('.listPanel__itemSubtitle:visible:contains("' + title + '")').first()
         .parent().parent().within(() => {
             cy.get('.pkpButton:contains("View")').click();
