23cf545a3b89527f049c20abf57e5793c061ae6b
Jhon <jhon@lepidus.com.br>
2025-05-05 16:38:08 -0400

    Adds integration test - Filter by moderation stage
    Issue: documentacao-e-tarefas/scielo#773
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- cypress/tests/Test4_submissionsPage.cy.js
+++ cypress/tests/Test4_submissionsPage.cy.js
@@ -1,8 +1,9 @@
 import '../support/commands.js';
 
-describe('SciELO Moderation Stages - Exhibitors on submissions page', function () {
+describe('SciELO Moderation Stages - Features on submissions page', function () {
     let submission1 = "Night of the Living Dead";
-    let submission2 = "Ju-on: The Grudge";
+    let submission2 = 'Candyman';
+    let submission3 = "Ju-on: The Grudge";
     
     before(function() {
         Cypress.config('defaultCommandTimeout', 4000);
@@ -18,7 +19,7 @@ describe('SciELO Moderation Stages - Exhibitors on submissions page', function (
                 cy.contains('Area Moderation');
             });
 
-        cy.get('.listPanel__itemSubtitle:visible:contains("' + submission2 + '")')
+        cy.get('.listPanel__itemSubtitle:visible:contains("' + submission3 + '")')
             .parent().within(() => {
                 cy.contains('Moderation stage:');
                 cy.contains('Manuscript Type Pre-Moderation');
@@ -37,12 +38,39 @@ describe('SciELO Moderation Stages - Exhibitors on submissions page', function (
                 cy.contains('Submission made less than a day ago');
             });
 
-        cy.get('.listPanel__itemSubtitle:visible:contains("' + submission2 + '")')
+        cy.get('.listPanel__itemSubtitle:visible:contains("' + submission3 + '")')
             .parent().within(() => {
                 cy.contains('Moderation stage:');
                 cy.contains('Manuscript Type Pre-Moderation');
 
                 cy.contains('Submission made less than a day ago');
             });
+    });
+    it("Editor can filter submissions by moderation stage", function () {
+        cy.login('dbarnes', null, 'publicknowledge');
+
+        cy.get('#active-button').click();
+        cy.contains('.pkpButton', 'Filters').click();
+        cy.contains('h4', 'Moderation Stages');
+
+        cy.contains('.pkpFilter__label', 'Area Moderation').click();
+        cy.waitJQuery();
+        cy.get('li.listPanel__item:visible').should('have.length', 1);
+        cy.contains(submission1);
+
+        cy.contains('.pkpFilter__label', 'Manuscript Type Pre-Moderation').click();
+        cy.waitJQuery();
+        cy.get('li.listPanel__item:visible').should('have.length', 2);
+        cy.contains(submission1);
+        cy.contains(submission3);
+
+        cy.get('#archive-button').click();
+        cy.contains('.pkpButton', 'Filters').click();
+        cy.contains('h4', 'Moderation Stages');
+
+        cy.contains('.pkpFilter__label', 'Format Pre-Moderation').click();
+        cy.waitJQuery();
+        cy.get('li.listPanel__item:visible').should('have.length', 1);
+        cy.contains(submission2);
     });
 });
