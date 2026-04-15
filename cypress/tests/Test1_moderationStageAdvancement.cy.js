d5a4685ff67a270c80e00c01671c29b7d9bee29d
Jhon <jhon@lepidus.com.br>
2026-04-15 15:14:34 -0400

    Fixes assertion in integration test
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- cypress/tests/Test1_moderationStageAdvancement.cy.js
+++ cypress/tests/Test1_moderationStageAdvancement.cy.js
@@ -80,7 +80,7 @@ describe("SciELO Moderation Stages - Moderation stage advancement", function() {
         cy.get('#nav-tab button:contains("Text")').click();
 
         cy.contains('Your submission has been forwarded to the Manuscript Type Pre-Moderation stage');
-        cy.contains('To facilitate the moderation process, please provide an up-to-date ORCID that includes the most recent scholarly work for at least one of the authors listed in the submission');
+        cy.contains('To facilitate moderation, please provide an updated ORCID with the most recent scientific output for at least one of the authors registered in the submission');
         cy.contains('Optionally, you may also provide an endorsement for the preprint, if you have one');
         cy.contains('For more information, we recommend reading our FAQs #10 and #19');
     });
