a6685cc11f20dfa625473215b33e2f5b5d205158
Yves César <yves@lepidus.com.br>
2026-06-16 12:22:08 -0400

    Inline StageAdvancementEmailBuilder and remove unused base class
    The ModerationStageEmailBuilder base was extracted to share code with
    StageRegressionEmailBuilder, which was later removed. With a single
    subclass left, restore StageAdvancementEmailBuilder to its standalone
    form and drop the base class.

--- classes/mail/builders/StageAdvancementEmailBuilder.php
+++ classes/mail/builders/StageAdvancementEmailBuilder.php
@@ -2,6 +2,8 @@
 
 namespace APP\plugins\generic\scieloModerationStages\classes\mail\builders;
 
+use APP\core\Application;
+use APP\facades\Repo;
 use APP\submission\Submission;
 use PKP\mail\Mailable;
 use APP\plugins\generic\scieloModerationStages\classes\ModerationStage;
@@ -10,24 +12,54 @@ use APP\plugins\generic\scieloModerationStages\classes\mail\mailables\{
     SentToTypeModerationStage
 };
 
-class StageAdvancementEmailBuilder extends ModerationStageEmailBuilder
+class StageAdvancementEmailBuilder
 {
+    private $context;
+    private $submission;
+    private $primaryAuthor;
     private $moderationStage;
+    private $emailParams;
 
-    public function setSubmission(Submission $submission): static
+    public function setSubmission(Submission $submission): StageAdvancementEmailBuilder
     {
-        parent::setSubmission($submission);
+        $this->submission = $submission;
         $this->moderationStage = $submission->getData('currentModerationStage');
+        return $this;
+    }
+
+    public function buildEmailParams(): StageAdvancementEmailBuilder
+    {
+        $publication = $this->submission->getCurrentPublication();
+        $this->primaryAuthor = $publication->getPrimaryAuthor();
+        if (!isset($this->primaryAuthor)) {
+            $authors = $publication->getData('authors');
+            $this->primaryAuthor = $authors->first();
+        }
+
+        $this->context = Application::get()->getContextDAO()->getById($this->submission->getData('contextId'));
+        $request = Application::get()->getRequest();
+        $faqUrl = $request->url($this->context->getPath()) . '/faq';
+
+        $this->emailParams = [
+            'authorName' => htmlspecialchars($this->primaryAuthor->getFullName()),
+            'faqUrl' => $faqUrl
+        ];
+
         return $this;
     }
 
     public function build(): Mailable
     {
+        $context = Application::get()->getContextDAO()->getById($this->submission->getData('contextId'));
         list($emailTemplateKey, $emailTemplateClass) = $this->getEmailTemplateByModerationStage($this->moderationStage);
-        $emailTemplate = $this->getEmailTemplate($emailTemplateKey);
 
-        $email = new $emailTemplateClass($this->context, $this->submission, $this->emailParams);
-        $email->from($this->context->getData('contactEmail'), $this->context->getData('contactName'));
+        $emailTemplate = Repo::emailTemplate()->getByKey(
+            $context->getId(),
+            $emailTemplateKey
+        );
+
+        $email = new $emailTemplateClass($context, $this->submission, $this->emailParams);
+        $email->from($context->getData('contactEmail'), $context->getData('contactName'));
         $email->to([
             ['name' => $this->primaryAuthor->getFullName(), 'email' => $this->primaryAuthor->getEmail()]
         ]);
